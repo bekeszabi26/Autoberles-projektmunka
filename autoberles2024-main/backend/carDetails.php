8769922ed6923e1a5c475df530490c82f7868c37
Szabi <szabi@local.com>
2024-04-22 13:28:21 +0200

    update

--- autoberles2024-main/backend/carDetails.php
+++ autoberles2024-main/backend/carDetails.php
@@ -7,7 +7,14 @@ if(isset($_GET['carId'])){
     //a tesztelés során ellenőriztük, sikerült e eltárolni a változóba az adatot
     //var_dump($carId);
     // Lekérdezés az adatbázisból
-    $sql = "SELECT * FROM car WHERE CarID = $carId";
+    $sql = "SELECT car.CarID, car.Description, car.Picture, car.Color, car.Engine, car.Performance, car.Seats, car.ProductionYear, car.AirConditioning, car.Deposit, car.PricePerDay, brand.BrandName, carmodel.CarModelName, bodytype.BodyTypeName, fueltype.FuelName, drivetype.DriveName, transmissiontype.TransmissionType
+            FROM car
+            JOIN carmodel ON car.ModelID = carmodel.ModelID
+            JOIN brand ON carmodel.BrandID = brand.BrandID
+            JOIN bodytype ON car.BodyTypeID = bodytype.BodyTypeID
+            JOIN fueltype ON car.FuelID = fueltype.FuelID
+            JOIN drivetype ON car.DriveID = drivetype.DriveID
+            JOIN transmissiontype ON car.TransmissionID = transmissiontype.TransmissionID WHERE CarID = $carId";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
--- autoberles2024-main/carDetails.php
+++ autoberles2024-main/carDetails.php
@@ -1,26 +1,37 @@
 <?php
 session_start();
 include_once "backend/config.php";
+$out = "";
+$carId = "";
 if (isset($_SESSION['RenterID'])) {
     $renterId = $_SESSION['RenterID'];
-}
-//var_dump($renterId);
-if (isset($_GET["carId"])) {
-    $carId = $_GET["carId"];
-    // Most már a $carId változóban van a CarID értéke
-    //echo "CarID: " . $carId; ezzel ellenőriztem meg e kapom az ID-t
+    if (isset($_GET["carId"])) {
+        $carId = $_GET["carId"];
+        // Most már a $carId változóban van a CarID értéke
+        //echo "CarID: " . $carId; ezzel ellenőriztem meg e kapom az ID-t
+    }       
+    // Űrlap elküldése
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //print_r("fut"); ellenőrizem hogy lefut a függvény gombkattintásra
+        $startingDay = $_POST['startingDay'];
+        $endingDay = $_POST['endingDay'];
+        //print_r($startingDay); ellenőriztem hogy kezembe kerültek e az adatok
+        //print_r($endingDay);
+        $sql2 = mysqli_query($conn, "INSERT INTO reservation (RenterID, CarID, StartingDay, EndingDay)
+                VALUES ('{$renterId}', '{$carId}', '{$startingDay}', '{$endingDay}')");
+        $kimenet = "";
+        if ($sql2 > 0) {
+            $kimenet = "
+                        <p>Köszönjük a foglalást!</p>
+                        <p>Munkatársaink hamarosan felveszik Önnel a kapcsolatot Email-ben!</p>
+                        <p><a href=\"profilF.php\">Vissza a profil oldalra</a></p>";
+        }
+        
+    }
+}else{
+    $out = 'A foglaláshoz be kell jelentkeznie!';
 }
 
-// Űrlap elküldése
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //print_r("fut"); ellenőrizem hogy lefut a függvény gombkattintásra
-    $startingDay = $_POST['startingDay'];
-    $endingDay = $_POST['endingDay'];
-    //print_r($startingDay); ellenőriztem hogy kezembe kerültek e az adatok
-    //print_r($endingDay);
-    $sql2 = mysqli_query($conn, "INSERT INTO reservation (RenterID, CarID, StartingDay, EndingDay)
-            VALUES ('{$renterId}', '{$carId}', '{$startingDay}', '{$endingDay}')");
-}
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="css/cardetails.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 
 <body ng-controller="CarDetailsController">
@@ -67,14 +79,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <img class="car-image" ng-src="{{ picture }}" alt="Car Image">
                 </a>
             </div>
-            <!-- <p><h1>{{car.BrandName}} {{ car.CarModelName }}</h1></p> -->
+            <p><h1>{{car.BrandName}} {{ car.CarModelName }}</h1></p>
             <p><strong>Leírás:</strong> {{ car.Description }}</p>
             <p><strong>Szine:</strong> {{ car.Color }}</p>
+            <p><strong>Kivitel:</strong> {{ car.BodyTypeName }}</p>
             <p><strong>Motor:</strong> {{ car.Engine }} cm<sup>3</sup></p>
             <p><strong>Lőerő:</strong> {{ car.Performance }}</p>
+            <p><strong>Váltó:</strong> {{ car.TransmissionType }}</p>
+            <p><strong>Meghajtás:</strong> {{ car.DriveName }}</p>
+            <p><strong>Üzemanyag:</strong> {{ car.FuelName }}</p>
             <p><strong>Ülések száma:</strong> {{ car.Seats }}</p>
-            <p><strong>Gyártási év:</strong> {{ car.ProductionYear }}</p>
-            <!-- <p><strong>Váltó:</strong> {{ transmissiontype.TransmissionType }}</p> -->
+            <p><strong>Gyártási év:</strong> {{ car.ProductionYear }}</p>            
             <p><strong>Kaució:</strong> {{ car.Deposit }}Ft</p>
             <p><strong>Ár/Nap:</strong> {{ car.PricePerDay }}Ft</p>
             <!-- Foglalási időszak -->
@@ -83,16 +98,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <label for="endingDay">Foglalás vége:</label>
             <input type="date" id="endingDay" name="endingDay" min="<?php echo date('Y-m-d'); ?>" required><br>
-
-            <div id="reservationModal" class="modal">
-                <span class="close">&times;</span>
-                <div class="modal-content">
-                    <p>Köszönjük a foglalást!</p>
-                    <p>Munkatársaink hamarosan felveszik Önnel a kapcsolatot Email-ben!</p>
-                    <p><a href="profilF.php">Vissza a profil oldalra</a></p>
-                </div>
-            </div>
+            
+            <?php if (isset($kimenet)) { echo $kimenet; } ?>
+            
             <button type="submit" name="reserve" id="reserveButton">Foglalás</button>
+            <strong class="foglalalert"><?php echo $out; ?></strong>
         </div>
     </form>
 
@@ -145,9 +155,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 let startingDay = document.getElementById("startingDay").value;
                 let endingDay = document.getElementById("endingDay").value;
                 if (startingDay && endingDay) {
-                    modal.style.display = "block";
+                    //modal.style.display = "block";
                 } else {
-                    alert("Kérjük, töltse ki mindkét dátummezőt!");
+                    swal("Kérjük, töltse ki mindkét dátummezőt!");
                 }
             }
 
@@ -162,7 +172,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     modal.style.display = "none";
                 }
             }
+
         });
+        function salert()
+        {
+            swal({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success",
+            button: "Aww yiss!",
+            });
+        }
     </script>
 </body>
 
